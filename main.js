9e0b2b16a613c542da7b078956743827ab96aaa5
Valentýna Kociánová <123517319+entyna@users.noreply.github.com>
2023-02-04 01:22:50 +0100

    02

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ const renderer = new c2.Renderer(document.getElementById('c2'));
 resize();
 
 renderer.background('#cccccc');
-let random = new c2.Random();
 
+let random = new c2.Random();
 
 let world = new c2.World(new c2.Rect(0, 0, renderer.width, renderer.height));
 
-for(let i=0; i<300; i++){
+for(let i=0; i<900; i++){
   let x = random.next(renderer.width);
-  let y = renderer.height;
+  let y = random.next(renderer.height);
   let p = new c2.Particle(x, y);
   p.radius = random.next(1, 5);
-  p.color = c2.Color.hsl(30);
+  p.color = c2.Color.hsl(100);
 
   world.addParticle(p);
 }
@@ -20,17 +20,20 @@ for(let i=0; i<300; i++){
 let collision = new c2.Collision();
 world.addInteractionForce(collision);
 
-let lineField3 = new c2.LineField(new c2.Line(0, renderer.height*0.3, renderer.width, renderer.height*0.3), 1.5);
-world.addForce(lineField3);
-
-let lineField2a = new c2.LineField(new c2.Line(0, renderer.height*0.6, renderer.width*0.4, renderer.height*0.6), 0.5);
+let lineField2a = new c2.LineField(new c2.Line(renderer.width*0.2, renderer.height*0.3, renderer.width*0.4, renderer.height*0.3), -0.5);
 world.addForce(lineField2a);
 
-let lineField2b = new c2.LineField(new c2.Line(renderer.width*0.6, renderer.height*0.6, renderer.width, renderer.height*0.6), 0.5);
+let lineField2b = new c2.LineField(new c2.Line(renderer.width*0.6, renderer.height*0.3, renderer.width*0.8, renderer.height*0.3), -0.5);
 world.addForce(lineField2b);
 
-let lineField1 = new c2.LineField(new c2.Line(0, renderer.height*0.9, renderer.width, renderer.height*0.9), 0.4);
-world.addForce(lineField1);
+let lineField1a = new c2.LineField(new c2.Line(renderer.width*0.2, renderer.height*0.6, renderer.width*0.4, renderer.height*0.6), 0.5);
+world.addForce(lineField1a);
+
+let lineField1b = new c2.LineField(new c2.Line(renderer.width*0.6, renderer.height*0.6, renderer.width*0.8, renderer.height*0.6), 0.5);
+world.addForce(lineField1b);
+
+let constForce = new c2.ConstForce(0, -1);
+world.addForce(constForce);
 
 renderer.draw(() => {
     renderer.clear();
@@ -44,10 +47,11 @@ renderer.draw(() => {
       renderer.stroke('#333333');
       renderer.lineWidth(1);
       renderer.fill(p.color);
-      renderer.circle(p.position.x, p.position.y, p.radius);
-      renderer.lineWidth(2);
-    //   renderer.point(p.position.x, p.position.y);
+    //   renderer.circle(p.position.x, p.position.y, p.radius);
+    //   renderer.lineWidth(2);
+      renderer.point(p.position.x, p.position.y);
     }
+
 });
 
 
