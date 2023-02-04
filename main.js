78bca7de4172da82d12727386e0f47701661091b
Valentýna Kociánová <123517319+entyna@users.noreply.github.com>
2023-02-04 18:58:03 +0100

    WIND

--- main.js
+++ main.js
@@ -1,42 +1,33 @@
 let world;
-// let perlin = new c2.Perlin();
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    //colorMode(HSL, 360, 100, 100);
     ellipseMode(RADIUS);
 
-    // world = new c2.World(new c2.Rect(0, 0, width, height));
     world = new c2.World();
-
-    // for(let i=0; i<700; i++){
-    //     let x = random(width);
-    //     let y = random(height);
-    //     let p = new c2.Particle(x, y);
-    //     p.radius = 2;
-    //     p.color = color(0);
-
-    //     world.addParticle(p);
-    // }
 
     let collision = new c2.Collision();
     world.addInteractionForce(collision);
 
-    // let constForce = new c2.ConstForce(0, -1);
-    // world.addForce(constForce);
+    let constForce = new c2.ConstForce(0, -1);
+    world.addForce(constForce);
+    
 
-    let lineField3 = new c2.LineField(new c2.Line(0, height*0.25, width, height*0.25), -2);
-    world.addForce(lineField3);
+    let yaoYang2 = new c2.LineField(new c2.Line(width*0.25, height*0.25, width*0.75, height*0.25), -0.5);
+    world.addForce(yaoYang2);
+    yaoYang2.range(0, 200);
    
-    let lineField2 = new c2.LineField(new c2.Line(0, height*0.5, width, height*0.5), -2);
-    world.addForce(lineField2);
+    let yaoYang1 = new c2.LineField(new c2.Line(width*0.25, height*0.5, width*0.75, height*0.5), -1);
+    world.addForce(yaoYang1);
+    yaoYang1.range(0, 200);
 
-    let lineField1a = new c2.PointField(new c2.Point(width*0.25, height*0.75), 1);
-    world.addForce(lineField1a);
+    let yaoYinA = new c2.LineField(new c2.Line(width*0.25, height*0.75, width*0.4, height*0.75), 1);
+    world.addForce(yaoYinA);
+    yaoYinA.range(0, 200);
 
-    let lineField1b = new c2.PointField(new c2.Point(width*0.75, height*0.75), 1);
-    world.addForce(lineField1b);
-
+    let yaoYinB = new c2.LineField(new c2.Line(width*0.6, height*0.75, width*0.75, height*0.75), 1);
+    world.addForce(yaoYinB);
+    yaoYinB.range(0, 200);
 
     noLoop();
     setInterval(function() {
@@ -73,5 +64,6 @@ function draw() {
     }
 
     world.update();
+
 
 }
