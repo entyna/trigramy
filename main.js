998b4ca8ddccb80d7f11c64c5d0c791faf9c1e97
Valentýna Kociánová <123517319+entyna@users.noreply.github.com>
2023-02-04 14:15:50 +0100

    HEAVEN

--- main.js
+++ main.js
@@ -22,20 +22,27 @@ function setup() {
     let collision = new c2.Collision();
     world.addInteractionForce(collision);
 
-    let constForce = new c2.ConstForce(0, -1);
-    world.addForce(constForce);
+    // let constForce = new c2.ConstForce(0, -0.1);
+    // world.addForce(constForce);
 
-    let lineField3 = new c2.LineField(new c2.Line(width*0.2, height*0.25, width*0.8, height*0.25), -0.5);
-    world.addForce(lineField3);
+    let lineField3a = new c2.LineField(new c2.Line(width*0.2, height*0.25, width*0.4, height*0.25), -1);
+    world.addForce(lineField3a);
 
-    let lineField2a = new c2.LineField(new c2.Line(width*0.2, height*0.5, width*0.4, height*0.5), 0.5);
+    let lineField3b = new c2.LineField(new c2.Line(width*0.6, height*0.25, width*0.8, height*0.25), -1);
+    world.addForce(lineField3b);
+
+    let lineField2a = new c2.LineField(new c2.Line(width*0.2, height*0.5, width*0.4, height*0.5), -0.1);
     world.addForce(lineField2a);
 
-    let lineField2b = new c2.LineField(new c2.Line(width*0.6, height*0.5, width*0.8, height*0.5), 0.5);
+    let lineField2b = new c2.LineField(new c2.Line(width*0.6, height*0.5, width*0.8, height*0.5), -0.1);
     world.addForce(lineField2b);
 
-    let lineField1 = new c2.LineField(new c2.Line( width*0.2, height*0.75,  width*0.8, height*0.75), -0.3 );
-    world.addForce(lineField1);
+    let lineField1a = new c2.LineField(new c2.Line(width*0.2, height*0.75, width*0.4, height*0.75), -1);
+    world.addForce(lineField1a);
+
+    let lineField1b = new c2.LineField(new c2.Line(width*0.6, height*0.75, width*0.8, height*0.75), -1);
+    world.addForce(lineField1b);
+
 
     noLoop();
     setInterval(function() {
